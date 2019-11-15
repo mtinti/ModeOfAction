e6ba529f8d7fd4235a17e9fc4eb7ebb0bf24a61b
mtinti <michele.tinti@gmail.com>
2019-11-15 13:10:27 +0000

    multiple fastq, region viz

--- make_pipline.py
+++ make_pipline.py
@@ -7,36 +7,80 @@ Created on Thu Jun 19 11:52:11 2014
 import os
 
 replace_list = eval(open('vars.txt').read())
-template_file = open('_template_mapping.sh').read()
+
 template_R = open('_template_count.R').read()
 #print(template_file)
 if __name__ == '__main__':
-    run_all_content = ''
-    for dictionary in replace_list:
-        sh_script_name = dictionary['base_fastq']+'.sh'
-        #run_all_content+='dos2unix '+sh_script_name+'\n'
-        run_all_content+='chmod +x '+sh_script_name+'\n'
-        run_all_content+='qsub '+sh_script_name+'\n'
-        run_all_content+='mv '+sh_script_name+' '+dictionary['experiment']+'\n'
+	run_all_content = ''
+	fast_q_files = []
+	for dictionary in replace_list:
+		fast_q_files.append(dictionary['base_fastq'])
+		sh_script_name = dictionary['base_fastq']+'.sh'
+		#run_all_content+='dos2unix '+sh_script_name+'\n'
+		run_all_content+='chmod +x '+sh_script_name+'\n'
+		run_all_content+='qsub '+sh_script_name+'\n'
+		run_all_content+='mv '+sh_script_name+' '+dictionary['experiment']+'\n'
         
-
-        sh_script_content = template_file.format(
-                g_version=dictionary['g_version'],
-                base_fastq=dictionary['base_fastq'],
-                experiment=dictionary['experiment'],
-                library=dictionary['library'])
-        path_to_bam = os.path.join(dictionary['experiment'],'data',dictionary['base_fastq'])
-        file_list = ['.sorted.bam','ff_barcode_sorted.bam','fr_barcode_sorted.bam','rf_barcode_sorted.bam','rr_barcode_sorted.bam']
-        r_count = template_R.format(
-        count_file = os.path.join(dictionary['experiment'], dictionary['base_fastq']+'count.txt'),
-        gtf_file = os.path.join('genomes', dictionary['g_version'], dictionary['g_version']+'.gtf'),
-        bam_files = ',\n'.join([ '\"'+os.path.join(path_to_bam, dictionary['base_fastq']+n)+'\"' for n in file_list ])
-        )
-        open('count_'+dictionary['base_fastq']+'.R','w').write(r_count)
-        open(sh_script_name,'w').write(sh_script_content)
+		template_file = open('_template_mapping_multiple.sh').read()
+		sh_script_content = template_file.format(
+			g_version=dictionary['g_version'],
+			base_fastq=dictionary['base_fastq'],
+			experiment=dictionary['experiment'],
+			library=dictionary['library'],
+			remove_1='##',
+			remove_2='##',
+			reads_1='',
+			reads_2=''
+			)
+		path_to_bam = os.path.join(dictionary['experiment'],'data',dictionary['base_fastq'])
+		file_list = ['.sorted.bam','ff_barcode_sorted.bam','fr_barcode_sorted.bam','rf_barcode_sorted.bam','rr_barcode_sorted.bam']
+		r_count = template_R.format(
+		count_file = os.path.join(dictionary['experiment'], dictionary['base_fastq']+'count.txt'),
+		gtf_file = os.path.join('genomes', dictionary['g_version'], dictionary['g_version']+'.gtf'),
+		bam_files = ',\n'.join([ '\"'+os.path.join(path_to_bam, dictionary['base_fastq']+n)+'\"' for n in file_list ])
+		)
+		open('count_'+dictionary['base_fastq']+'.R','w').write(r_count)
+		open(sh_script_name,'w').write(sh_script_content)
         
-    run_all_content+='mv '+'run_all_'+dictionary['experiment']+'.sh'+' '+dictionary['experiment']+'\n'
-    open('run_all_'+dictionary['experiment']+'.sh','w').write(run_all_content)
     
+ 
+
+	if len(fast_q_files) > 1:
+		reads_1=[]
+		reads_2=[]
+		path = os.path.join(dictionary['experiment'],'data')
+		reads_1 = ' '.join([os.path.join(path,n+'1.fq.gz') for n in fast_q_files])
+		reads_2 = ' '.join([os.path.join(path,n+'2.fq.gz') for n in fast_q_files])
+		base_fastq = 'merge_'
+		print(dictionary['library'])
+		template_file = open('_template_mapping_multiple.sh').read()
+		sh_script_content = template_file.format(
+					g_version=dictionary['g_version'],
+					base_fastq=base_fastq,
+					experiment=dictionary['experiment'],
+					library=dictionary['library'],
+					remove_1='',
+					remove_2='',
+					reads_1=reads_1,
+					reads_2=reads_2)
+					
+		path_to_bam = os.path.join(dictionary['experiment'],'data','merge_')
+		file_list = ['.sorted.bam','ff_barcode_sorted.bam','fr_barcode_sorted.bam','rf_barcode_sorted.bam','rr_barcode_sorted.bam']
+			
+		r_count = template_R.format(
+			count_file = os.path.join(dictionary['experiment'], 'merge_'+'count.txt'),
+			gtf_file = os.path.join('genomes', dictionary['g_version'], dictionary['g_version']+'.gtf'),
+			bam_files = ',\n'.join([ '\"'+os.path.join(path_to_bam, 'merge_'+n)+'\"' for n in file_list ])
+			)
+		open('count_merge.R','w').write(r_count)
+		sh_script_name='merge_and_run.sh'
+		open(sh_script_name,'w').write(sh_script_content)
+
+		run_all_content+='chmod +x '+sh_script_name+'\n'
+		run_all_content+='qsub '+sh_script_name+'\n'
+		run_all_content+='mv '+sh_script_name+' '+dictionary['experiment']+'\n'
+
+	run_all_content+='mv '+'run_all_'+dictionary['experiment']+'.sh'+' '+dictionary['experiment']+'\n'	
+	open('run_all_'+dictionary['experiment']+'.sh','w').write(run_all_content)
 
     
---
+++ plot_reagion_coverage.py
@@ -0,0 +1,126 @@
+import sys
+import pandas as pd
+import matplotlib.pyplot as plt
+from matplotlib.patches import Rectangle
+from matplotlib.collections import PatchCollection
+import gc
+from matplotlib.patches import Rectangle
+import matplotlib.pyplot as plt
+import matplotlib.gridspec as gridspec
+plt.style.use('ggplot')
+from adjustText import adjust_text
+import numpy as np
+
+#plt.style.use('classic')
+def get_id(col):
+    return col.split(';')[0].split('=')[1]
+        
+def gff_to_pandas(infile):
+  
+    df = pd.read_csv(infile, sep='\t', header=None, index_col=None,comment='#')
+    df.columns = ['chro','source','ftype','start',
+                  'end', 'score', 'strand', 'score2', 'desc']
+    df=df[df['ftype']=='gene']
+    df['id']=[get_id(n) for n in df['desc']]
+    return df
+
+def add_track(bed_file = str, chro = str, 
+              start =int, end=int, ax=None,
+              label = str, color=str, roll=False):
+    df = pd.read_csv(bed_file, sep='\t', header=None,index_col=None)
+    if bed_file.endswith('_d.bed'):
+        df.columns = ['chr', 'base', 'coverage']
+    if bed_file.endswith('_bg.bed'):
+        df.columns = ['chr', 'base', 'to_base', 'coverage']
+        
+    test = df[df['chr']==chro]
+    del df
+    gc.collect()
+    if roll:
+        test['coverage']=test['coverage']-test['coverage'].mean()
+        test['coverage']=test['coverage'].rolling(window=2000).median()
+        #test=test[test['coverage']>0]
+
+    test=test[ (test['base']>start) & (test['base']<end)]    
+    test.plot(x='base', y='coverage', ax=ax, label=label, c=color)
+    return test.base.min(), test.base.max()
+
+def plot_region(coverage_file, ff_file, fr_file, rf_file, rr_file, chro, start, end, save_to):
+
+    f = plt.figure(figsize=(16,6))
+    gs = gridspec.GridSpec(2, 1, height_ratios=[3, 1])
+    ax = plt.subplot(gs[0])
+    axt = ax.twinx()
+
+    _, _, = add_track(bed_file = coverage_file, chro = chro, 
+              start =start, end=end, ax=ax, label='coverage',color='b', roll=True)
+
+
+    ax.set_ylabel('Coverage')
+    axt.set_ylabel('Primer Coverage')
+
+
+    region = gff[(gff['chro']==chro) & (gff['start'] > start ) & (gff['end'] < end )]
+
+    ax.set_xlim(start, end)
+    ax.set_ylim(bottom=0)
+    
+    ax2 = plt.subplot(gs[1], sharex = ax)
+
+    add_track(bed_file = ff_file, chro = chro, 
+              start =start, end=end, ax=axt, label='ff',color='r')
+
+    add_track(bed_file = fr_file, chro = chro, 
+              start =start, end=end, ax=axt, label='fr',color='g')
+
+
+    add_track(bed_file = rf_file, chro = chro, 
+              start =start, end=end, ax=axt, label='rf',color='y')
+
+    add_track(bed_file = rr_file, chro = chro, 
+              start =start, end=end, ax=axt, label='rr',color='k')
+
+
+    ax.legend(loc="upper left")
+
+
+
+    for gene in region.index.values:
+        gstart = region.loc[gene].start
+        gend = region.loc[gene].end
+        ax2.add_patch(Rectangle((gstart, 0), width=gend-gstart, height=0.1))
+
+    texts = [ax2.text(region.loc[i].start, 
+                           0.1,
+                           region.loc[i]['id'])
+                           for i in region.index.values]  
+
+    x= np.arange(start, end, 100) 
+    y=[0.2 for n in x]
+    ax2.scatter(x, y,alpha=0)
+    ax2.set_ylim(0,1)
+    adjust_text(texts, arrowprops=dict(arrowstyle='->', color='red'), ax=ax2)
+    title = '{chro}:{start}-{end}'.format(chro=chro,start=start,end=end)
+    ax.set_title(title)
+    plt.savefig(save_to+title+'.png')
+    
+if __name__ == -__main__':
+
+    gff = sys.argv[1]
+    gff = gff_to_pandas(gff)
+    gff.head()
+    
+    peaks = pd.read_table(sys.argv[2], sep='\t', comment='#')
+    peaks = peaks.sort_values('pileup', ascending=False)
+
+    coverage_file = sys.argv[3]
+    ff_file = sys.argv[4]
+    fr_file = sys.argv[5]
+    rf_file = sys.argv[6]
+    rr_file = sys.argv[7]
+    save_to = sys.argv[8]
+    for peak in peaks.index.values[0:3]:
+        chro = df.loc[peak]['chr']
+        start = df.loc[peak]['start']
+        end = df.loc[peak]['end']
+        plot_region(coverage_file, ff_file, fr_file, rf_file, rr_file, chro, start, end, save_to)
